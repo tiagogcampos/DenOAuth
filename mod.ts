97db48d78c41a1e2fba2fcb5c599a6b8c1b7d5b7
Tiago Campos <13822700+tiagogcampos@users.noreply.github.com>
2022-09-13 18:57:15 +0100

    add facebook strategy export

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
-export { LinkedInClient } from './LinkedInStrategy/linkedIn_client.ts';
-export { GitHubClient } from './GitHubStrategy/GitHub_client.ts';
-export { GoogleClient } from './GoogleStrategy/Google_client.ts';
-export { SpotifyClient} from './SpotifyStrategy/spotify_client.ts';
-export { DiscordClient} from './DiscordStrategy/Discord_client.ts';
+export { LinkedInClient } from "./LinkedInStrategy/linkedIn_client.ts";
+export { GitHubClient } from "./GitHubStrategy/GitHub_client.ts";
+export { GoogleClient } from "./GoogleStrategy/Google_client.ts";
+export { SpotifyClient } from "./SpotifyStrategy/spotify_client.ts";
+export { DiscordClient } from "./DiscordStrategy/Discord_client.ts";
+export { FacebookClient } from "./FacebookStrategy/Facebook_client.ts";
